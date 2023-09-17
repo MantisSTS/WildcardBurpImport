f9a47be18e87370edaefbb364ba30192faf81100
MantisSTS <rclifford@cybershade.org>
2023-09-17 08:37:22 +0100

    Pushing first commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # WildcardBurpImport
 A tool to convert a list of wildcard domains into Burp's scope
+
+## Install
+
+```
+go install -v github.com/MantisSTS/WildcardBurpImport@latest
+```
+
+## Usage
+
+**wildcards.txt**
+
+```txt
+yahoo.com
+google.com
+```
+
+```bash
+$ WildcardBurpImport -f wildcards.txt -o results.json
+```
---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+)
+
+type ScopeImport struct {
+	Target struct {
+		Scope struct {
+			AdvancedMode bool  `json:"advanced_mode"`
+			Exclude      []any `json:"exclude,omitempty"`
+			Include      []struct {
+				Enabled  bool   `json:"enabled"`
+				Host     string `json:"host"`
+				Protocol string `json:"protocol"`
+			} `json:"include"`
+		} `json:"scope"`
+	} `json:"target"`
+}
+
+func main() {
+	input := flag.String("f", "", "The wildcard file to parse")
+	output := flag.String("o", "", "The output file to write to")
+	flag.Parse()
+
+	if *input == "" || *output == "" {
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	file, err := os.Open(*input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	var scope ScopeImport
+	scope.Target.Scope.AdvancedMode = true
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+
+		wildcard := scanner.Text()
+		wildcard = regexp.QuoteMeta(strings.TrimSpace(wildcard))
+
+		scope.Target.Scope.Include = append(scope.Target.Scope.Include, struct {
+			Enabled  bool   `json:"enabled"`
+			Host     string `json:"host"`
+			Protocol string `json:"protocol"`
+		}{Enabled: true, Host: wildcard, Protocol: "any"})
+	}
+
+	b, err := json.MarshalIndent(scope, "", "\t")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = os.WriteFile(*output, b, 0644)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
